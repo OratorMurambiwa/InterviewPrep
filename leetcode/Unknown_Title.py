c9d6cec5a995248a8566358875937156258cb6bf
OratorMurambiwa <murambiwaorator@gmail.com>
2025-11-06 22:35:01 -0600

    Add or update Unknown_Title

--- leetcode/Unknown_Title.py
+++ leetcode/Unknown_Title.py
@@ -1,22 +1,22 @@
 # Unknown_Title
-# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/submissions/1822978699/?envType=problem-list-v2&envId=oizxjoit
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/?envType=problem-list-v2&envId=oizxjoit
 # Language: Unknown
 # Difficulty: Medium
 
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-class Solution(object):
-    def lowestCommonAncestor(self, root, p, 
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, 
 q):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: TreeNode
-        """
-        if not root:
-            return None
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
