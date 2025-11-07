5486786790617b0392c2bb280d9be0780513342c
OratorMurambiwa <murambiwaorator@gmail.com>
2025-11-06 22:11:15 -0600

    Add Untitled

---
+++ leetcode/Untitled_1762488676390.py
@@ -0,0 +1,15 @@
+# Untitled
+# https://leetcode.com/problems/word-break/submissions/1822967777/?envType=problem-list-v2&envId=e4set62i
+# Language: Unknown
+# Difficulty: Medium
+
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        """
+        Understand
+        Inputs
