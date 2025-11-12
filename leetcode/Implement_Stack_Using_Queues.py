f03f14851980ef4c3dfe8f88d9381e321fdad599
OratorMurambiwa <murambiwaorator@gmail.com>
2025-11-11 23:06:41 -0600

    Add or update Implement Stack Using Queues

---
+++ leetcode/Implement_Stack_Using_Queues.py
@@ -0,0 +1,15 @@
+# Implement Stack Using Queues
+# https://neetcode.io/problems/implement-stack-using-queues?list=neetcode250
+# Language: Python
+# Difficulty: Unspecified
+
+class MyStack:
+
+    def __init__(self):
+        
+
+    def push(self, x: int) -> None:
+        
+
+    def pop(self) -> int:
+        
