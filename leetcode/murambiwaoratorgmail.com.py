8bf9b6a7ac856cb5c206d4c658fbd8fa782770cd
OratorMurambiwa <murambiwaorator@gmail.com>
2025-11-12 08:42:11 -0600

    Add or update murambiwaorator@gmail.com

--- leetcode/murambiwaoratorgmail.com.py
+++ leetcode/murambiwaoratorgmail.com.py
@@ -3,13 +3,6 @@
 # Language: Python
 # Difficulty: Unspecified
 
+        
 
-        while l <= r:
-            m = (l+r)//2
-
-            if guess(m) == 0:
-                return m
-            elif guess(m) < 0:
-                r = m - 1
-            else:
-                l = m + 1
+        
