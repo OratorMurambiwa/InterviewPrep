8c1f259dcb47dca819a2f2c44777c283d0891332
OratorMurambiwa <murambiwaorator@gmail.com>
2025-11-12 08:19:15 -0600

    Add or update Search Insert Position

---
+++ leetcode/Search_Insert_Position.py
@@ -0,0 +1,8 @@
+# Search Insert Position
+# https://neetcode.io/problems/search-insert-position?list=neetcode250
+# Language: Python
+# Difficulty: Unspecified
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        
