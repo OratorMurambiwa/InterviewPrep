9b3b51f3f1491d9beafcf6dd922e56d0d84c4406
OratorMurambiwa <murambiwaorator@gmail.com>
2025-12-29 03:03:27 -0600

    Create 189-RotateArray

---
+++ 189-RotateArray/189-RotateArray.py
@@ -0,0 +1,25 @@
+# Last updated: 29/12/2025, 03:03:28
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        # k = k%n
+        # def reverse(start, end):
+        #     while start < end:
+        #         temp = nums[start]
+        #         nums[start] = nums[end]
+        #         nums[end] = temp
+        #         start += 1
+        #         end -= 1
+
+        # reverse(0, n-1)
+        # reverse(0, k-1)
+        # reverse(k, n-1)
+
+        k = k % n
+        nums[:] = nums[-k:] + nums[:-k]
+        
