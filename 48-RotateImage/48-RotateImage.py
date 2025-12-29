f917c25143deb5da1c0a5e52a4795ee8c399b705
OratorMurambiwa <murambiwaorator@gmail.com>
2025-12-29 03:04:00 -0600

    Create 48-RotateImage

---
+++ 48-RotateImage/48-RotateImage.py
@@ -0,0 +1,16 @@
+# Last updated: 29/12/2025, 03:04:01
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix) 
+
+        for r in range(n):
+            for c in range(r+1, n):
+                matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
+
+        for r in range(n):
+            for c in range(n//2):
+                matrix[r][c], matrix[r][n-c-1] = matrix[r][n-c-1], matrix[r][c]
