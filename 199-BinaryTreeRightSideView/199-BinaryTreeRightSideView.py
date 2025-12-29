638a64964b6257a8e723f9c79e308f04dc5085e7
OratorMurambiwa <murambiwaorator@gmail.com>
2025-12-29 03:03:26 -0600

    Create 199-BinaryTreeRightSideView

---
+++ 199-BinaryTreeRightSideView/199-BinaryTreeRightSideView.py
@@ -0,0 +1,37 @@
+# Last updated: 29/12/2025, 03:03:27
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+
+        result = []
+        queue = deque([root])
+
+        while queue:
+            size = len(queue)
+
+            for i in range(size):
+                node = queue.popleft()
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+                if i == size - 1:
+                    result.append(node.val)
+
+        return result
+
+
+        
