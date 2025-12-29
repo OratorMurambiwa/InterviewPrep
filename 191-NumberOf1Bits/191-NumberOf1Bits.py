0109c117a38e1e7dee314f18eb31e91f33393bbd
OratorMurambiwa <murambiwaorator@gmail.com>
2025-12-29 03:03:25 -0600

    Create 191-NumberOf1Bits

---
+++ 191-NumberOf1Bits/191-NumberOf1Bits.py
@@ -0,0 +1,14 @@
+# Last updated: 29/12/2025, 03:03:26
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count = 0
+        while n > 0:
+            n = n & (n-1)
+            count += 1
+
+        return count
+        
