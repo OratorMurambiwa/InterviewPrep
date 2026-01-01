cba21c252829c89a8ca3fa7128aea6c624aa3e67
OratorMurambiwa <murambiwaorator@gmail.com>
2025-12-31 21:30:31 -0600

    File updated at 31/12/2025, 21:30:33

--- 566-ReshapetheMatrix/566-ReshapetheMatrix.py
+++ 566-ReshapetheMatrix/566-ReshapetheMatrix.py
@@ -1,29 +1,27 @@
-# Last updated: 31/12/2025, 20:45:11
+# Last updated: 31/12/2025, 21:30:33
 1class Solution(object):
-2    def longestPalindrome(self, s):
+2    def longestPalindrome(self, words):
 3        """
-4        :type s: str
+4        :type words: List[str]
 5        :rtype: int
 6        """
-7        freq = defaultdict(int)
-8
-9        for char in s:
-10            freq[char] += 1
-11        
-12        length = 0
-13        odd = False
-14
-15        for count in freq.values():
-16            if count % 2 == 0:
-17                length += count
-18            else:
-19                length += count -1
-20                odd = True
-21        
-22        if odd:
-23            return length + 1
-24        else:
-25            return length
-26
-27
-28        
+7        count = Counter(words)
+8        length = 0
+9        center = False
+10
+11        for word, freq in count.items():
+12            rev = word[::-1]
+13
+14            if word == rev:
+15                length += (freq//2) * 4
+16                if freq % 2 == 1:
+17                    center = True
+18
+19            elif word < rev:
+20                pairs = min(freq, count[rev])
+21                length += pairs * 4
+22
+23        if center:
+24            length +=2
+25
+26        return length
