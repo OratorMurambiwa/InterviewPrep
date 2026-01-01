8e4c7f3bff0414174f01d0d1ddc49ab33b9d1ab2
OratorMurambiwa <murambiwaorator@gmail.com>
2025-12-31 18:43:42 -0600

    Create 890-LemonadeChange

---
+++ 890-LemonadeChange/890-LemonadeChange.py
@@ -0,0 +1,24 @@
+# Last updated: 31/12/2025, 18:43:44
+class Solution(object):
+    def lemonadeChange(self, bills):
+        """
+        :type bills: List[int]
+        :rtype: bool
+        """
+        five = ten = 0
+
+        for b in bills:
+            if b == 5:
+                five += 1
+            elif b == 10:
+                five, ten = five - 1, ten + 1
+            else:
+                if ten > 0:
+                    five, ten = five - 1, ten-1
+                else:
+                    five -=3
+            if five < 0:
+                return False
+            
+        return True
+        
