c849c8bc185eec1116c8ab50a39f03361161c2db
OratorMurambiwa <murambiwaorator@gmail.com>
2026-01-01 04:44:06 -0600

    File updated at 01/01/2026, 04:44:07

--- 566-ReshapetheMatrix/566-ReshapetheMatrix.py
+++ 566-ReshapetheMatrix/566-ReshapetheMatrix.py
@@ -1,31 +1,28 @@
-# Last updated: 01/01/2026, 04:01:48
+# Last updated: 01/01/2026, 04:44:07
 1class Solution(object):
-2    def combinationSum(self, candidates, target):
+2    def coinChange(self, coins, amount):
 3        """
-4        :type candidates: List[int]
-5        :type target: int
-6        :rtype: List[List[int]]
+4        :type coins: List[int]
+5        :type amount: int
+6        :rtype: int
 7        """
-8        res = []
-9
-10        def backtrack(i, current, total):
-11            if total == target:
-12                res.append(list(current))
-13                return
-14
-15            if i >= len(candidates) or total > target:
-16                return
-17
-18            current.append(candidates[i])
-19            backtrack(i, current, total + candidates[i])
-20            current.pop()
-21
-22            backtrack(i + 1, current, total)
-23
-24        backtrack(0, [], 0)
-25        return res
-26
-27
-28
-29
-30        
+8        dp = [0] * (amount+1)
+9        coins.sort()
+10
+11        for i in range(1, amount + 1):
+12            minn = float('inf')
+13
+14            for coin in coins:
+15                diff = i - coin
+16
+17                if diff < 0:
+18                    break
+19                minn = min(minn, dp[diff] + 1)
+20
+21            dp[i] = minn
+22
+23        if dp[amount] < float('inf'):
+24            return dp[amount]
+25        else:
+26            return -1
+27        
