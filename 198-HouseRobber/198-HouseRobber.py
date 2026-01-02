68ae27020800ec9de2080df5d1a3d74f3eccff88
OratorMurambiwa <murambiwaorator@gmail.com>
2026-01-01 19:50:37 -0600

    File updated at 01/01/2026, 19:50:36

--- 198-HouseRobber/198-HouseRobber.py
+++ 198-HouseRobber/198-HouseRobber.py
@@ -1,24 +1,17 @@
-# Last updated: 29/12/2025, 03:03:25
-class Solution(object):
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        if not nums:
-            return 0
-
-        if len(nums)==1:
-            return nums[0]
-
-        prev = curr = 0
-
-        for num in nums:
-            prev, curr = curr, max(curr, prev + num)
-        
-        return curr
-        
-
-
-
-        
+# Last updated: 01/01/2026, 19:50:36
+1class Solution(object):
+2    def rob(self, nums):
+3        """
+4        :type nums: List[int]
+5        :rtype: int
+6        """
+7        prevh = 0
+8        prev2h = 0
+9
+10        for i in nums:
+11            new_total = max(prevh, prev2h + i)
+12            prev2h = prevh
+13            prevh = new_total
+14
+15        return prevh
+16        
