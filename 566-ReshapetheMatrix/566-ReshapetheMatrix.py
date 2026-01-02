b4a3ec509ef3296746b42ab39c6d2bfae7356fbd
OratorMurambiwa <murambiwaorator@gmail.com>
2026-01-02 07:32:07 -0600

    File updated at 02/01/2026, 07:32:07

--- 566-ReshapetheMatrix/566-ReshapetheMatrix.py
+++ 566-ReshapetheMatrix/566-ReshapetheMatrix.py
@@ -1,28 +1,13 @@
-# Last updated: 01/01/2026, 04:44:07
+# Last updated: 02/01/2026, 07:32:07
 1class Solution(object):
-2    def coinChange(self, coins, amount):
+2    def singleNumber(self, nums):
 3        """
-4        :type coins: List[int]
-5        :type amount: int
-6        :rtype: int
-7        """
-8        dp = [0] * (amount+1)
-9        coins.sort()
-10
-11        for i in range(1, amount + 1):
-12            minn = float('inf')
-13
-14            for coin in coins:
-15                diff = i - coin
-16
-17                if diff < 0:
-18                    break
-19                minn = min(minn, dp[diff] + 1)
-20
-21            dp[i] = minn
-22
-23        if dp[amount] < float('inf'):
-24            return dp[amount]
-25        else:
-26            return -1
-27        
+4        :type nums: List[int]
+5        :rtype: int
+6        """
+7        numset = Counter(nums)
+8
+9        for i in nums:
+10            if numset[i] == 1:
+11                return i 
+12        
