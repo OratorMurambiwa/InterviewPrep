2d091b32d52919d31f682df46301c612dd8af757
OratorMurambiwa <murambiwaorator@gmail.com>
2026-01-04 21:14:19 -0600

    File updated at 04/01/2026, 21:14:17

--- 566-ReshapetheMatrix/566-ReshapetheMatrix.py
+++ 566-ReshapetheMatrix/566-ReshapetheMatrix.py
@@ -1,13 +1,33 @@
-# Last updated: 02/01/2026, 07:32:07
+# Last updated: 04/01/2026, 21:14:17
 1class Solution(object):
-2    def singleNumber(self, nums):
+2    def findPairs(self, nums, k):
 3        """
 4        :type nums: List[int]
-5        :rtype: int
-6        """
-7        numset = Counter(nums)
-8
-9        for i in nums:
-10            if numset[i] == 1:
-11                return i 
-12        
+5        :type k: int
+6        :rtype: int
+7        """
+8        # if k < 0:
+9        #     return 0
+10
+11        freq = defaultdict(int)
+12
+13        for x in nums:
+14            freq[x] += 1
+15
+16        count = 0
+17        
+18        if k == 0:
+19            for x in freq:
+20                if freq[x] > 1:
+21                    count += 1
+22            return count
+23
+24        for x in freq:
+25            if x + k in freq:
+26                count += 1
+27        return count
+28
+29
+30
+31
+32
