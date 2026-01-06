e857c6c66a541fa57641fdfe4d44fd296449da7c
OratorMurambiwa <murambiwaorator@gmail.com>
2026-01-05 23:04:26 -0600

    File updated at 05/01/2026, 23:04:25

--- 566-ReshapetheMatrix/566-ReshapetheMatrix.py
+++ 566-ReshapetheMatrix/566-ReshapetheMatrix.py
@@ -1,34 +1,16 @@
-# Last updated: 05/01/2026, 13:51:29
+# Last updated: 05/01/2026, 23:04:25
 1class Solution(object):
-2    def numberOfSubarrays(self, nums, k):
+2    def twoSum(self, nums, target):
 3        """
 4        :type nums: List[int]
-5        :type k: int
-6        :rtype: int
+5        :type target: int
+6        :rtype: List[int]
 7        """
-8        res = 0
-9        l = 0
-10        m = 0
-11        odd = 0
-12
-13        for r in range(len(nums)):
-14            if nums[r] % 2:
-15                odd += 1
-16                m = l  
-17
-18            while odd > k:
-19                if nums[l] % 2:
-20                    odd -= 1
-21                l += 1
-22                m = l  
-23
-24            if odd == k:
-25                while m <= r and nums[m] % 2 == 0:
-26                    m += 1
-27                res += (m - l + 1)
-28
-29        return res
-30
-31
-32
-33
+8        seen = {}
+9
+10        for i, num in enumerate(nums):
+11            complement = target - num
+12            if complement in seen:
+13                return [seen[complement], i]
+14            seen[num] = i
+15
