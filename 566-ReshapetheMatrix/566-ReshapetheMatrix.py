2ac6490ed010315c222218423f680739e8699962
OratorMurambiwa <murambiwaorator@gmail.com>
2026-01-05 23:18:07 -0600

    File updated at 05/01/2026, 23:18:06

--- 566-ReshapetheMatrix/566-ReshapetheMatrix.py
+++ 566-ReshapetheMatrix/566-ReshapetheMatrix.py
@@ -1,16 +1,34 @@
-# Last updated: 05/01/2026, 23:04:25
+# Last updated: 05/01/2026, 23:18:06
 1class Solution(object):
-2    def twoSum(self, nums, target):
+2    def threeSum(self, nums):
 3        """
 4        :type nums: List[int]
-5        :type target: int
-6        :rtype: List[int]
-7        """
-8        seen = {}
+5        :rtype: List[List[int]]
+6        """
+7        nums.sort()
+8        res = []
 9
-10        for i, num in enumerate(nums):
-11            complement = target - num
-12            if complement in seen:
-13                return [seen[complement], i]
-14            seen[num] = i
+10        for i in range(len(nums)):
+11            if i > 0 and nums[i] == nums[i-1]:
+12                continue
+13
+14            l, r = i + 1, len(nums) - 1
 15
+16            while l < r:
+17                total = nums[i] + nums[l] + nums[r]
+18                if total < 0:
+19                    l +=1
+20                elif total > 0:
+21                    r -= 1
+22                else:
+23                    res.append([nums[i], nums[l], nums[r]])
+24
+25                    l +=1
+26                    r -= 1
+27                    while l < r and nums[l] == nums[l-1]:
+28                        l += 1
+29                    while l < r and nums[r] == nums[r+1]:
+30                        r -= 1
+31
+32        return res
+33        
