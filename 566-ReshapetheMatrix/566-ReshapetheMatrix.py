19ea9862004dad6643ad0990912cd6f699fc4ca8
OratorMurambiwa <murambiwaorator@gmail.com>
2026-01-07 14:10:25 -0600

    File updated at 07/01/2026, 14:10:25

--- 566-ReshapetheMatrix/566-ReshapetheMatrix.py
+++ 566-ReshapetheMatrix/566-ReshapetheMatrix.py
@@ -1,20 +1,18 @@
-# Last updated: 07/01/2026, 13:56:25
+# Last updated: 07/01/2026, 14:10:25
 1class Solution(object):
-2    def merge(self, intervals):
+2    def eraseOverlapIntervals(self, intervals):
 3        """
 4        :type intervals: List[List[int]]
-5        :rtype: List[List[int]]
+5        :rtype: int
 6        """
 7        intervals.sort()
-8
-9        res = [intervals[0]]
+8        count = 0
+9        lastend = intervals[0][1]
 10
 11        for start, end in intervals[1:]:
-12            lastend = res[-1][1]
-13            if start <= lastend:
-14                res[-1][1] = max(lastend, end)
-15            else:
-16                res.append([start, end])
-17
-18        return res
-19
+12            if start >= lastend:
+13                lastend = end
+14            else:
+15                count +=1
+16                lastend = min(end, lastend)
+17        return count
