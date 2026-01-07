a1e658b59ca5b847748e635599397b200e4d3cc6
OratorMurambiwa <murambiwaorator@gmail.com>
2026-01-07 13:37:58 -0600

    File updated at 07/01/2026, 13:37:58

--- 566-ReshapetheMatrix/566-ReshapetheMatrix.py
+++ 566-ReshapetheMatrix/566-ReshapetheMatrix.py
@@ -1,34 +1,25 @@
-# Last updated: 05/01/2026, 23:18:06
+# Last updated: 07/01/2026, 13:37:58
 1class Solution(object):
-2    def threeSum(self, nums):
+2    def insert(self, intervals, newInterval):
 3        """
-4        :type nums: List[int]
-5        :rtype: List[List[int]]
-6        """
-7        nums.sort()
-8        res = []
-9
-10        for i in range(len(nums)):
-11            if i > 0 and nums[i] == nums[i-1]:
-12                continue
-13
-14            l, r = i + 1, len(nums) - 1
-15
-16            while l < r:
-17                total = nums[i] + nums[l] + nums[r]
-18                if total < 0:
-19                    l +=1
-20                elif total > 0:
-21                    r -= 1
-22                else:
-23                    res.append([nums[i], nums[l], nums[r]])
-24
-25                    l +=1
-26                    r -= 1
-27                    while l < r and nums[l] == nums[l-1]:
-28                        l += 1
-29                    while l < r and nums[r] == nums[r+1]:
-30                        r -= 1
-31
-32        return res
-33        
+4        :type intervals: List[List[int]]
+5        :type newInterval: List[int]
+6        :rtype: List[List[int]]
+7        """
+8        
+9        res = []
+10
+11        for i in range(len(intervals)):
+12            if newInterval[1] < intervals[i][0]:
+13                res.append(newInterval)
+14                return res + intervals[i:]
+15            elif newInterval[0] > intervals[i][1]:
+16                res.append(intervals[i])
+17            else:
+18                newInterval = [
+19                    min(newInterval[0], intervals[i][0]),
+20                    max(newInterval[1], intervals[i][1])
+21                ]
+22        res.append(newInterval)
+23
+24        return res
