f400d7fc65bfda06d3c928b27c9b9439809fab48
OratorMurambiwa <murambiwaorator@gmail.com>
2026-01-19 11:14:55 -0600

    File updated at 19/01/2026, 11:14:56

--- 18-4Sum/18-4Sum.py
+++ 18-4Sum/18-4Sum.py
@@ -1,40 +1,25 @@
-# Last updated: 19/01/2026, 10:56:07
+# Last updated: 19/01/2026, 11:14:56
 1class Solution(object):
-2    def fourSum(self, nums, target):
+2    def fourSumCount(self, nums1, nums2, nums3, nums4):
 3        """
-4        :type nums: List[int]
-5        :type target: int
-6        :rtype: List[List[int]]
-7        """
-8        nums.sort()
-9        res = []
-10        n = len(nums)
-11
-12        for i in range(n-3):
-13            if i > 0 and nums[i] == nums[i-1]:
-14                continue
-15
-16            for j in range(i+1, n-2):
-17                if j > i+1 and nums[j] == nums[j-1]:
-18                    continue
-19
-20                l, r = j+1, n-1
-21                while l < r:
-22                    sum = nums[i] + nums[j] + nums[l] + nums[r]
-23
-24                    if sum == target:
-25                        res.append([nums[i], nums[j], nums[l], nums[r]])
-26                        l +=1
-27                        r-=1
-28
-29                        while l < r and nums[l] == nums[l-1]:
-30                            l+=1
-31                        while l < r and nums[r] == nums[r+1]:
-32                            r-=1
-33                    
-34                    elif sum < target:
-35                        l+=1
-36                    else:
-37                        r -=1
-38        return res
-39            
+4        :type nums1: List[int]
+5        :type nums2: List[int]
+6        :type nums3: List[int]
+7        :type nums4: List[int]
+8        :rtype: int
+9        """
+10        sumAB = defaultdict(int)
+11        res = 0
+12
+13        for a in nums1:
+14            for b in nums2:
+15                total = a + b
+16                sumAB[total] +=1
+17        
+18        for c in nums3:
+19            for d in nums4:
+20                cd_total = c + d
+21                needed = -cd_total
+22                res += sumAB[needed]
+23        
+24        return res
